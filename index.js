4ef31909151359e330401a012419a2384d0a6902
Vahe Israyelyan <vahe.israyelyan@gmail.com>
2017-09-15 16:00:26 +0400

    test

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ RODIN.Scene.HMDCamera.focalLength = 10;
 const POINTS = {};
 
 function deleteHotspots() {
-    
+    console.log("test");
 }
 
 function loadHotspots() {
-    
+
 }
 
 function loadPoint(image_path, hotspots, point_id) {
-    
+
     var imagePrefix = image_path;
     var directions  = ["r", "l", "u", "d", "f", "b"];
     var imageSuffix = ".jpg";
     var storiesGeometry = new THREE.CubeGeometry( 100, 100, 100 );
-    
+
     var materialArray = [];
     for (let i = 0; i < 6; i++) {
     	materialArray.push( new THREE.MeshBasicMaterial({
@@ -33,11 +33,11 @@ function loadPoint(image_path, hotspots, point_id) {
     storiesBox.rotation.y = Math.PI;
     const exp360_scene1 = new RODIN.Sculpt();
     POINTS[point_id].hotspots = [];
-    
+
     for(let i = 0; i < hotspots.length; i++) {
-        
+
         POINTS[point_id].hotspots[i] = {};
-        
+
         POINTS[point_id].hotspots[i].id = hotspots[i].id;
         POINTS[point_id].hotspots[i].ath = hotspots[i].ath;
         POINTS[point_id].hotspots[i].atv = hotspots[i].atv;
@@ -45,7 +45,7 @@ function loadPoint(image_path, hotspots, point_id) {
         POINTS[point_id].hotspots[i].vlookat = hotspots[i].vlookat;
         POINTS[point_id].hotspots[i].to_id = hotspots[i].to_point.id;
         POINTS[point_id].hotspots[i].to_id_name = hotspots[i].to_point.point_name;
-        
+
         if(hotspots[i].custom_image == null) {
             POINTS[point_id].hotspots[i].plane = new RODIN.Plane(5, 2.77, new THREE.MeshBasicMaterial({ map: RODIN.Loader.loadTexture("images/hotspot.png"), transparent: true }));
         }
@@ -54,13 +54,13 @@ function loadPoint(image_path, hotspots, point_id) {
             POINTS[point_id].hotspots[i].plane.on("RODIN.CONST.READY", (e) => {
                 e.target.material.map.anisotropy = 16;
                 e.target.material.map.anisotropy = 16;
-                
+
             })
         }
-        
+
         POINTS[point_id].hotspots[i].hotspotHolderY = new RODIN.Sculpt();
         POINTS[point_id].hotspots[i].hotspotHolderX = new RODIN.Sculpt();
-        
+
         POINTS[point_id].hotspots[i].hotspotHolderY.add(POINTS[point_id].hotspots[i].hotspotHolderX);
         exp360_scene1.add(POINTS[point_id].hotspots[i].hotspotHolderY);
         POINTS[point_id].hotspots[i].hotspotHolderX.add(POINTS[point_id].hotspots[i].plane);
@@ -68,27 +68,27 @@ function loadPoint(image_path, hotspots, point_id) {
         POINTS[point_id].hotspots[i].hotspotHolderX.rotation.x= -(Math.PI / 180) * hotspots[i].atv;
         POINTS[point_id].hotspots[i].plane.position.set(0, 0, -36);
         POINTS[point_id].hotspots[i].plane.rotation.x = -Math.PI / 5;
-        
-        
+
+
 
     }
-    
+
     for(let f = 0; f < POINTS[point_id].hotspots.length; f++) {
-        
+
         POINTS[point_id].hotspots[f].plane.on(RODIN.CONST.GAMEPAD_BUTTON_DOWN, function (evt) {
             RODIN.Scene.remove(exp360_scene1);
             loadXMLDoc(POINTS[point_id].hotspots[f].to_id);
         });
-        
+
     }
-    
+
     console.log(POINTS);
-    
+
     RODIN.messenger.once(RODIN.CONST.ALL_SCULPTS_READY, ()=> {
         exp360_scene1.add(storiesBox);
         RODIN.Scene.add(exp360_scene1);
     });
-    
+
 }
 
 
@@ -107,7 +107,7 @@ function loadXMLDoc(query) {
                    hotspots.push(answer.response.spots[i]);
                }
                loadPoint(imagePath, hotspots, query);
-               
+
            }
            else if (xmlhttp.status == 400) {
               alert('There was an error 400');
@@ -124,5 +124,3 @@ function loadXMLDoc(query) {
 }
 
 loadXMLDoc(6778);
-
-    
